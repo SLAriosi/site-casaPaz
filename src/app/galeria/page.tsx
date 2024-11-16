41682d774d5c099eff92263a0e2bd6473cd0472b
AriosiLucas <ariosilucas@gmail.com>
2024-11-16 15:26:18 -0300

    Finaliza página de galeria

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,15 +1,27 @@
 "use client"
-import { Box, Container, Flex, Heading, Link, Stack, Text, Button } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading, Link, Stack, Text, } from '@chakra-ui/react';
 import Image from 'next/image';
-import { useState } from 'react';
-import desfile_1 from '../../../public/images/DESFILE_1.jpg';
-import desfile_2 from '../../../public/images/DESFILE_2.jpg';
-import desfile_3 from '../../../public/images/DESFILE_3.jpg';
-import desfile_4 from '../../../public/images/DESFILE_4.jpg';
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const IMAGE_URL = process.env.NEXT_PUBLIC_IMAGE_URL;
+
+interface EventoImage {
+   evento_id: number;
+   url: string;
+}
+interface Eventos {
+   id: number;
+   nome: string;
+   descricao: string;
+}
 
 export default function Galeria() {
    const [isModalOpen, setIsModalOpen] = useState(false);
    const [selectedImage, setSelectedImage] = useState('');
+   const [imagensEventos, setImagensEventos] = useState<EventoImage[]>([]);
+   const [eventos, setEventos] = useState<Eventos[]>([]);
 
    const handleImageClick = (src: string) => {
       setSelectedImage(src);
@@ -21,124 +33,81 @@ export default function Galeria() {
       setSelectedImage('');
    };
 
+   useEffect(() => {
+      const fetchCards = async () => {
+         try {
+            const response = await axios.get(`${API_URL}/imagem-eventos`);
+
+            const data = response.data;
+            setImagensEventos(data);
+
+         } catch (error) {
+            console.error('Error fetching cards:', error);
+         }
+      };
+
+      const fetchEventos = async () => {
+         try {
+            const response = await axios.get(`${API_URL}/eventos`);
+
+            const data = response.data;
+            setEventos(data);
+
+         } catch (error) {
+            console.error('Error fetching cards:', error);
+         }
+      };
+
+      fetchEventos();
+      fetchCards();
+   }, []);
+
    return (
       <Box bg="gray.50" minH="100vh">
          {/* Cabeçalho */}
          <Box bg="#4970b1" color="white" py={3} textAlign="center">
             <Heading fontSize="2xl">Eventos</Heading>
             <Flex justify="center" mt={4} gap={4}>
-               <Link href="#pdi" fontWeight="bold">Desfile Kids</Link>-
-               <Link href="#pif" fontWeight="bold">PIF</Link>-
-               <Link href="#pdc" fontWeight="bold">PDC</Link>
+               {eventos.map((evento) => (
+                  <Link key={evento.id} href={`#${evento.nome.toLowerCase().replace(/\s+/g, '')}`} fontWeight="bold">
+                     {evento.nome}
+                  </Link>
+               ))}
             </Flex>
          </Box>
 
-         <Container maxW="container.lg" py={10}>
-            <Stack mb={3} textAlign="center">
-               <h2 className="text-2xl sm:text-2xl md:text-3xl font-extrabold pb-3" style={{ color: "#4970b1" }}>
-                  Desfile Kids
-               </h2>
-               <Text fontSize="lg" color="gray.600" maxW="600px" mx="auto">
-                  Programa de Desenvolvimento Individual - Um caminho para o crescimento pessoal e profissional, focado em alcançar metas e objetivos específicos.
-               </Text>
+         <Container maxW="container.xl" py={10}>
+            {eventos.map((evento) => (
+               <Stack key={evento.id} mb={3} textAlign="center">
+                  <h2 id={evento.nome.toLowerCase().replace(/\s+/g, '')} className="text-2xl sm:text-2xl md:text-3xl font-extrabold pb-3" style={{ color: "#4970b1" }}>
+                     {evento.nome}
+                  </h2>
+                  <Text fontSize="lg" color="gray.600" maxW="800px" mx="auto" textAlign="justify" mb={5}>
+                     {evento.descricao}
+                  </Text>
 
-               <Flex direction="row" justify="center" align="center" wrap="nowrap" gap={12} py={5}>
-                  <Image
-                     src={desfile_1}
-                     alt="Imagem 1"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_1.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-                  <Image
-                     src={desfile_2}
-                     alt="Imagem 2"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_2.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-                  <Image
-                     src={desfile_3}
-                     alt="Imagem 3"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_3.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-                  <Image
-                     src={desfile_4}
-                     alt="Imagem 4"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_4.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-               </Flex>
-               <Flex direction="row" justify="center" align="center" wrap="nowrap" gap={12} py={5}>
-                  <Image
-                     src={desfile_1}
-                     alt="Imagem 1"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_1.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-                  <Image
-                     src={desfile_2}
-                     alt="Imagem 2"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_2.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-                  <Image
-                     src={desfile_3}
-                     alt="Imagem 3"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_3.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-                  <Image
-                     src={desfile_4}
-                     alt="Imagem 4"
-                     width={150}
-                     height={150}
-                     objectFit="cover"
-                     style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
-                     onClick={() => handleImageClick(desfile_4.src)}
-                     onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
-                     onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
-                  />
-               </Flex>
-            </Stack>
+                  <Flex direction="row" justify="center" align="center" wrap="wrap" gap={12} py={5}>
+                     {imagensEventos.filter(imagem => imagem.evento_id === evento.id).map((imagem, index) => (
+                        <Image
+                           key={`${imagem.evento_id}-${index}`}
+                           src={`${IMAGE_URL}${imagem.url}`}
+                           alt={`Imagem ${imagem.evento_id}`}
+                           width="150"
+                           height="300"
+                           objectFit="cover"
+                           style={{ borderRadius: '8px', cursor: 'pointer', transition: 'filter 0.3s' }}
+                           onClick={() => handleImageClick(`${IMAGE_URL}/${imagem.url}`)}
+                           onMouseOver={(e) => (e.currentTarget.style.filter = 'brightness(0.8)')}
+                           onMouseOut={(e) => (e.currentTarget.style.filter = 'brightness(1)')}
+                        />
+                     ))}
+                  </Flex>
+                  <Flex justify="center" my={4} pb={4}>
+                     <Box bg="#a2bd30" height="6px" width="15%" />
+                  </Flex>
+               </Stack>
+            ))}
             <Box borderBottom="1px" borderColor="gray.200" my={4} />
-            <Flex justify="center" my={4}>
-               <Box bg="#a2bd30" height="6px" width="15%" />
-            </Flex>
          </Container>
 
          {isModalOpen && (
